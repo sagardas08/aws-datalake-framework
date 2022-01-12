c396703f392ad6115f6358ce8b4d6a77757523dd
Suprakash Nandy <suprakash.nandy@tigeranalytics.com>
2022-01-12 12:17:52 +0530

    Cleanup and rearranged function in utils

--- src/utils/comUtils.py
+++ src/utils/comUtils.py
@@ -1,4 +1,18 @@
-import json, decimal
+import json
+import decimal
+import boto3
+import pydeequ
+from pyspark.sql import SparkSession
+
+# utility function that returns spark object
+def get_spark():
+  spark = (SparkSession
+    .builder
+    .config("spark.jars.packages", pydeequ.deequ_maven_coord)
+    .config("spark.jars.excludes", pydeequ.f2j_maven_coord)
+    .getOrCreate())
+  return spark
+
 
 def create_spark_df(
   spark, source_file_path, asset_file_type,
@@ -6,13 +20,13 @@ def create_spark_df(
 ):
   if asset_file_type == 'csv' and asset_file_header == True:
     source_df = spark.read.csv(
-      path=source_file_path, sep = asset_file_delim,
+      path=source_file_path, sep=asset_file_delim,
       header=True,  inferSchema=True
     )
   elif asset_file_type == 'csv' and asset_file_header == False:
     source_df = spark.read.csv(
-      path=source_file_path, 
-      sep = asset_file_delim
+      path=source_file_path,
+      sep=asset_file_delim
     )
   elif asset_file_type == "parquet":
     source_df = spark.read.parquet(source_file_path)
@@ -25,17 +39,41 @@ def create_spark_df(
     return source_df
   except AssertionError:
     print("Unable to read the data from the source")
-    
+
+
 # Helper class to convert a DynamoDB item to JSON.
 class DecimalEncoder(json.JSONEncoder):
   def default(self, o):
     if isinstance(o, decimal.Decimal):
       return str(o)
-    if isinstance(o, set):  #<---resolving sets as lists
+    if isinstance(o, set):  # <---resolving sets as lists
       return list(o)
     return super(DecimalEncoder, self).default(o)
+
 
 def dynamodbJsonToDict(dynamodbJson):
   for i in dynamodbJson['Items']:
     items = json.dumps(i, cls=DecimalEncoder)
   return json.loads(items)
+
+
+# utility function to store Pandas DF to S3
+def store_to_s3(data_type, bucket, key, data):
+  if data_type == 'json':
+    s3 = boto3.client('s3')
+    json_object = data
+    s3.put_object(
+        Body=json.dumps(json_object),
+        Bucket=bucket,
+        Key=key
+    )
+  elif data_type == 'dataframe':
+    csv_buffer = StringIO()
+    data.to_csv(csv_buffer)
+    s3_resource = boto3.resource('s3')
+    try:
+      s3_resource.Object(bucket, key).put(Body=csv_buffer.getvalue())
+      print(
+        "stored DF to bucket -> {0} with key -> {1}".format(bucket, key))
+    except Exception as e:
+      print(e)
--- src/utils/dqUtils.py
+++ src/utils/dqUtils.py
@@ -1,4 +1,5 @@
-import json, decimal
+import json
+import decimal
 import pydeequ
 from pydeequ.analyzers import *
 from pydeequ.profiles import *
@@ -7,23 +8,42 @@ from pydeequ.checks import *
 from pydeequ.verification import *
 from pydeequ.repository import *
 
+def run_profiler(spark, df):
+  print("Running Profiler")
+  result = ColumnProfilerRunner(spark) \
+    .onData(df) \
+    .run()
+  for col, profile in result.profiles.items():
+    print(profile)
+
+
+def run_constraint_suggestion(spark, df):
+  print("Running constraint suggestions")
+  suggestionResult = ConstraintSuggestionRunner(spark) \
+    .onData(df) \
+    .addConstraintRule(DEFAULT()) \
+    .run()
+  # Json format
+  return json.dumps(suggestionResult, indent=2)
+
+
 def run_constraint_checks(spark, df, return_type='pandas'):
   print("Running constraint checks")
   check = Check(spark, CheckLevel.Warning, "Execute Data Quality Checks")
   checkResult = VerificationSuite(spark) \
-  .onData(df) \
-  .addCheck(
-    check.hasSize(lambda x: x >= 3000000) \
+    .onData(df) \
+    .addCheck(check.hasSize(lambda x: x >= 3000000) \
     .hasMin("star_rating", lambda x: x == 1.0) \
-    .hasMax("star_rating", lambda x: x == 5.0)  \
-    .isComplete("review_id")  \
-    .isUnique("review_id")  \
-    .isComplete("marketplace")  \
+    .hasMax("star_rating", lambda x: x == 5.0) \
+    .isComplete("review_id") \
+    .isUnique("review_id") \
+    .isComplete("marketplace") \
     .isNonNegative("year")) \
-  .run()
+    .run()
   print(f"Verification Run Status: {checkResult.status}")
   if return_type.lower() == 'spark':
     result = VerificationResult.checkResultsAsDataFrame(spark, checkResult)
   else:
-    result = VerificationResult.checkResultsAsDataFrame(spark, checkResult, pandas=True)
+    result = VerificationResult.checkResultsAsDataFrame(
+      spark, checkResult, pandas=True)
   return result
