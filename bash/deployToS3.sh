b6307b193435aad49f57306815e96572a7860c4f
Sagar Das <sagar.das@tigeranalytics.com>
2022-01-21 12:06:29 +0530

    Dynamic checks updated and tested

--- bash/deployToS3.sh
+++ bash/deployToS3.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+  echo "Error: Missing region argument"
+  exit 1
+fi
+
+export region=$1
 export script_path=$(readlink -f "$0")
 export project_name="aws-datalake-framework"
 export project_parent_dir=$(echo ${script_path} | awk -F "${project_name}" '{print $1}')
 
 cd ${project_parent_dir}
+rm -rf ${project_name}-temp
 cp -r ${project_name} ${project_name}-temp
 
 cd ${project_name}-temp/src
@@ -12,25 +19,26 @@ zip -r utils.zip utils
 mv utils.zip ${project_parent_dir}/${project_name}-temp/dependencies/
 cd ${project_parent_dir}
 
-aws s3 rm s3://dl-fmwrk-code --recursive
-aws s3 cp ${project_name}-temp s3://dl-fmwrk-code/${project_name} --recursive
+aws s3 rm s3://dl-fmwrk-code-${region} --recursive
+aws s3 cp ${project_name}-temp s3://dl-fmwrk-code-${region}/${project_name} --recursive
 rm -rf ${project_parent_dir}/${project_name}-temp
-#aws glue create-job \
-#  --name "2482-demo" \
-#  --role "2482-misc-service-role" \
-#  --command '{ \
-#    "Name": "glueetl", \
-#    "ScriptLocation": "s3://dl-fmwrk-code/aws-datalake-framework/src/genericDqChecks.py" \
-#    }' \
-#  --extra-py-files "s3://dl-fmwrk-code/aws-datalake-framework/dependencies/pydeequ.zip, \
-#    s3://dl-fmwrk-code/aws-datalake-framework/dependencies/utils.zip" \
-#  --extra-jars "s3://dl-fmwrk-code/aws-datalake-framework/dependencies/deequ-1.0.3.jar" \
-#  --TempDir "s3://dl-fmwrk-10000/temporary/" \
-#  --glue-version "2.0" \
-#  --number-of-workers 10 \
-#  --worker-type "G.2X" \
-#  --enable-metrics \
-#  --enable-continuous-cloudwatch-log \
-#  --region us-east-2
+
+aws glue delete-job --job-name dl-fmwrk-data-quality-checks --region $region
+aws glue create-job \
+  --name dl-fmwrk-data-quality-checks \
+  --role 2482-misc-service-role \
+  --command "{ \
+    \"Name\": \"glueetl\", \
+    \"ScriptLocation\": \"s3://dl-fmwrk-code-$region/aws-datalake-framework/src/genericDqChecks.py\" \
+    }" \
+  --default-arguments "{ \
+    \"--extra-py-files\": \"s3://dl-fmwrk-code-$region/aws-datalake-framework/dependencies/pydeequ.zip,s3://dl-fmwrk-code-$region/aws-datalake-framework/dependencies/utils.zip\", \
+    \"--extra-jars\": \"s3://dl-fmwrk-code-$region/aws-datalake-framework/dependencies/deequ-1.0.3.jar\", \
+    \"--TempDir\": \"s3://dl-fmwrk-10000-$region/temporary/\" \
+    }"\
+  --glue-version 2.0 \
+  --number-of-workers 10 \
+  --worker-type G.2X \
+  --region $region
 
 exit 0
--- src/genericDqChecks.py
+++ src/genericDqChecks.py
@@ -1,80 +1,60 @@
-import json, ast, decimal, sys, logging
+import sys
+
+from boto3.dynamodb.conditions import Key
+from awsglue.utils import getResolvedOptions
 
 from utils.comUtils import *
 from utils.dqUtils import *
-from io import StringIO
 
-import boto3
-from boto3.dynamodb.conditions import Key
-from pyspark.sql import SparkSession, Row
+spark = get_spark()
 
-import pydeequ
-from pydeequ.analyzers import *
-from pydeequ.profiles import *
-from pydeequ.suggestions import *
-from pydeequ.checks import *
-from pydeequ.verification import *
-from pydeequ.repository import *
+'''
+Capturing source information parameters from the step function. Parameters:
+  1. source_path: S3 path where the source file is created/landed
+  2. source_id: Source system identifier which is a random number associated
+    to the onboarded source. This identifier is also suffixed in the S3 
+    bucket name.
+  3. asset_id: Asset Identifier which is a random number associated to the 
+    onboadred data asset.
+'''
+args = getResolvedOptions(sys.argv, ['source_path', 'source_id', 'asset_id'])
+source_path = args['source_path']
+source_id = args['source_id']
+asset_id = args['asset_id']
 
-import sys
-from awsglue.utils import getResolvedOptions
-
-def get_spark():
-  spark = (SparkSession
-    .builder
-    .config("spark.jars.packages", pydeequ.deequ_maven_coord)
-    .config("spark.jars.excludes", pydeequ.f2j_maven_coord)
-    .getOrCreate())
-  return spark
-
-def main():
-  spark = get_spark()
-
-  '''
-  Capturing source information parameters from the step function. Parameters:
-    1. source_path: S3 path where the source file is created/landed
-    2. source_id: Source system identifier which is a random number associated
-      to the onboarded source. This identifier is also suffixed in the S3 
-      bucket name.
-    3. asset_id: Asset Identifier which is a random number associated to the 
-      onboadred data asset.
-  '''
-  args = getResolvedOptions(sys.argv, ['source_path', 'source_id', 'asset_id'])
-  source_path = args['source_path']
-  source_id = args['source_id']
-  asset_id = args['asset_id']
-
-  '''
-  Pulling the data asset information from dynamoDB table dl_fmwrk.data_asset 
-  based on the Asset ID
-  '''
-  dynamodb = boto3.resource('dynamodb')
-  asset_info = dynamodb.Table('dl_fmwrk.data_asset')
-  asset_info_items = asset_info.query(
+'''
+Pulling the data asset information from dynamoDB table dl_fmwrk.data_asset 
+based on the Asset ID
+'''
+region = 'us-east-2'
+dynamodb = boto3.resource('dynamodb', region_name=region)
+asset_info = dynamodb.Table('dl_fmwrk.data_asset')
+asset_info_items = asset_info.query(
     KeyConditionExpression=Key('asset_id').eq(int(asset_id))
-  )
-  items = dynamodbJsonToDict(asset_info_items)
-    
-  asset_file_type = items['file_type']
-  asset_file_delim = items['file_delim']
-  asset_file_header = items['file_header']
-
-  '''
-  Create dataframe using the source data asset
-  '''
-  source_file_path = source_path.replace("s3://", "s3a://")
-  source_df = create_spark_df(
+)
+items = dynamodbJsonToDict(asset_info_items)
+asset_file_type = items['file_type']
+asset_file_delim = items['file_delim']
+asset_file_header = items['file_header']
+metadata_table = f"dl_fmwrk.data_asset.{asset_id}"
+'''
+Create dataframe using the source data asset
+'''
+source_file_path = source_path.replace("s3://", "s3a://")
+source_df = create_spark_df(
     spark,
     source_file_path,
     asset_file_type,
     asset_file_delim,
     asset_file_header
-  )
-
-  check_df = run_constraint_checks(spark, source_df)
-  source_df.printSchema()
-  spark.sparkContext._gateway.close()
-  spark.stop()
-
-if __name__ == '__main__':
-    main()
+)
+source_df.printSchema()
+metadata = get_metadata(metadata_table, region)
+dq_code = generate_code(metadata)
+check = Check(spark, CheckLevel.Warning, "Deequ Data Quality Checks")
+checkOutput = None
+print("Executing the code")
+exec(dq_code, globals())
+result = VerificationResult.checkResultsAsDataFrame(spark, checkOutput)
+result.show()
+stop_spark(spark)
