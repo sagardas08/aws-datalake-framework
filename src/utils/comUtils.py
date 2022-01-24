d9cbcf930e101e8b60a814089362a3d2e4c10f0c
vismaya-Kalaiselvan <97148551+vismaya-Kalaiselvan@users.noreply.github.com>
2022-01-24 19:05:52 +0530

    Update comUtils.py

--- src/utils/comUtils.py
+++ src/utils/comUtils.py
@@ -77,3 +77,52 @@ def store_to_s3(data_type, bucket, key, data):
         "stored DF to bucket -> {0} with key -> {1}".format(bucket, key))
     except Exception as e:
       print(e)
+      
+#Utility function to get secret key from secrets manager for tokenising in data masking      
+def get_secret():
+
+    secret_name = "cape_privacy_key"
+    region_name = "us-east-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS key.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+            key_value_pair=json.loads(secret)
+            key=key_value_pair["key"]
+            return key
+        else:
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
