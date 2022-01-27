598b7084e2d672b63399242dd7de899b7ba03115
vismaya-Kalaiselvan <97148551+vismaya-Kalaiselvan@users.noreply.github.com>
2022-01-27 16:11:49 +0530

    Update capeprivacyUtils.py

--- src/utils/capeprivacyUtils.py
+++ src/utils/capeprivacyUtils.py
@@ -12,11 +12,14 @@ def get_spark_for_masking():
     return sess
     
 def run_data_masking(spark,source_df,metadata,key):
+    tokenize = Tokenizer(key=key)
+    perturb_numeric = NumericPerturbation(dtype=dtypes.Integer, min=-10, max=10)
+    perturb_date = DatePerturbation(frequency=("YEAR", "MONTH", "DAY"), min=(-10, -5, -5), max=(10, 5, 5))
+    round_numeric = NumericRounding(dtype=dtypes.Float, precision=-3)
     for i in metadata:
       for a,b in i.items():
           if a=="req_tokenization" and b==True:
               col_name=i.get("col_nm")
-              tokenize = Tokenizer(key=key)
               source_df = source_df.withColumn(col_name, tokenize(functions.col(col_name)))
           if a=="req_redaction" and b==True:
               col_name=i.get("col_nm")
@@ -24,9 +27,15 @@ def run_data_masking(spark,source_df,metadata,key):
               redact_list.append(col_name)
               redact = ColumnRedact(columns=redact_list)
               df = redact(df)
-          if a=="req_tokenization" and b==True:
+          if a=="req_dateperturbation" and b==True:
               col_name=i.get("col_nm")
-              source_df = source_df.withColumn(col_name, tokenize(functions.col(col_name)))
+              source_df = source_df.withColumn(col_name, perturb_date(functions.col(col_name)))
+          if a=="req_numericperturbation" and b==True:
+              col_name=i.get("col_nm")
+              source_df = source_df.withColumn(col_name, perturb_numeric(functions.col(col_name)))
+          if a=="req_numericrounding" and b==True:
+              col_name=i.get("col_nm")
+              source_df = source_df.withColumn(col_name, round_numeric(functions.col(col_name))
     return source_df
        
     
