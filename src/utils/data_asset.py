218056930a12f4d3d1200b23f8ad2dc454eaa1b6
vismaya-Kalaiselvan <97148551+vismaya-Kalaiselvan@users.noreply.github.com>
2022-02-14 22:11:27 +0530

    Update data_asset.py

--- src/utils/data_asset.py
+++ src/utils/data_asset.py
@@ -10,9 +10,9 @@ from .validateSchema import validate_schema
 class DataAsset:
     def __init__(self, args, config, run_identifier):
         """
-
-        :param args:
-        :param config:
+        Defines a Data Asset and its properties
+        :param args: Run time arguments gathered from Step Function
+        :param config: The Global config file
         """
         self.asset_metadata = None
         self.asset_id = args["asset_id"]
@@ -23,6 +23,7 @@ class DataAsset:
         self.region = config["primary_region"]
         self.log_type = config["log_type"]
         self.secret_name = config['secret_name']
+        self.source_file_path = self.source_path.replace("s3://", "s3a://")
         self.logger = Logger(
             log_type=self.log_type,
             log_name=self.exec_id,
@@ -37,7 +38,6 @@ class DataAsset:
         self.asset_file_delim = items["file_delim"]
         self.asset_file_header = items["file_header"]
         self.target_id = items["target_id"]
-        self.source_file_path = self.source_path.replace("s3://", "s3a://")
         self.metadata_table = f"{self.fm_prefix}.data_asset.{self.asset_id}"
         self.data_catalog = f"{self.fm_prefix}.data_catalog.{self.asset_id}"
 
@@ -77,7 +77,7 @@ class DataAsset:
         self, dq_validation=None, data_masking=None, data_standardization=None
     ):
         """
-
+        Updates the data catalog in DynamoDB
         :param dq_validation:
         :param data_masking:
         :param data_standardization:
@@ -104,6 +104,11 @@ class DataAsset:
         table.put_item(Item=item)
 
     def validate_schema(self, source_df):
+        """
+        Method to return if an asset's schema is validated
+        :param source_df:
+        :return:
+        """
         schema_validation = validate_schema(
             self.asset_file_type,
             self.asset_file_header,
