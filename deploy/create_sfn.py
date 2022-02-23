d5f46832e2d1759ab3fe85bb954295e1e98b0b4d
vismaya-Kalaiselvan <97148551+vismaya-Kalaiselvan@users.noreply.github.com>
2022-02-23 22:25:30 +0530

    Update create_sfn.py

--- deploy/create_sfn.py
+++ deploy/create_sfn.py
@@ -3,6 +3,37 @@ import json
 import time
 
 
+def sfn_exists(client, arn):
+    response = client.list_state_machines()
+    for i in response["stateMachines"]:
+        if i["stateMachineArn"] == arn:
+            return True
+    return False
+
+
+def delete_state_machine(client, arn):
+    response = client.delete_state_machine(stateMachineArn=arn)
+    while sfn_exists(client, arn):
+        time.sleep(5)
+
+
+def create_state_machine(client, sfn_name, asl_definition):
+    response = client.create_state_machine(
+        name=sfn_name,
+        definition=json.dumps(asl_definition),
+        roleArn="arn:aws:iam::076931226898:role/service-role/StepFunctions-dl-fmwrk-data-pipeline-role-5a555edb",
+        type="STANDARD",
+        loggingConfiguration={
+            "level": "OFF",
+            "includeExecutionData": False,
+        },
+        tags=[
+            {"key": "project", "value": "aws-datalake-framework"},
+        ],
+        tracingConfiguration={"enabled": False},
+    )
+
+
 def create_step_function(config, region=None):
     """
     Creates step function
@@ -13,6 +44,8 @@ def create_step_function(config, region=None):
     region = config["primary_region"] if region is None else region
     prefix = config["fm_prefix"]
     acc_no = config["aws_account"]
+    sfn_name = f"{prefix}-data-pipeline"
+    arn = f"arn:aws:states:{region}:{acc_no}:stateMachine:{prefix}-data-pipeline"
     asl_definition = {
         "Comment": "A description of my state machine",
         "StartAt": "Data Quality Checks",
@@ -65,32 +98,10 @@ def create_step_function(config, region=None):
         },
     }
     client = boto3.client("stepfunctions", region_name=region)
-    try:
-        response = client.delete_state_machine(
-            stateMachineArn=f"arn:aws:states:{region}:{acc_no}:stateMachine:{prefix}-data-pipeline"
-        )
-        is_deleted = False
-        while is_deleted == False:
-            response = client.describe_state_machine(
-                stateMachineArn=f"arn:aws:states:{region}:{acc_no}:stateMachine:{prefix}-data-pipeline"
-            )
-            if response["status"] == "ACTIVE" or response["status"] == "DELETING":
-                time.sleep(5)
-            else:
-                is_deleted = True
-    except:
-        pass
-    response = client.create_state_machine(
-        name=f"{prefix}-data-pipeline",
-        definition=json.dumps(asl_definition),
-        roleArn="arn:aws:iam::076931226898:role/service-role/StepFunctions-dl-fmwrk-data-pipeline-role-5a555edb",
-        type="STANDARD",
-        loggingConfiguration={
-            "level": "OFF",
-            "includeExecutionData": False,
-        },
-        tags=[
-            {"key": "project", "value": "aws-datalake-framework"},
-        ],
-        tracingConfiguration={"enabled": False},
-    )
+    if sfn_exists(client, arn):
+        update = str(input("Do you want to update the step function: Y/N  "))
+        if update == "Y":
+            delete_state_machine(client, arn)
+            create_state_machine(client, sfn_name, asl_definition)
+    else:
+        create_state_machine(client, sfn_name, asl_definition)
