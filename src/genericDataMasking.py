0daa087bafd1a70d08ea1009dab9dd7bbd5f9ffa
vismaya-Kalaiselvan <97148551+vismaya-Kalaiselvan@users.noreply.github.com>
2022-02-28 15:03:52 +0530

    Update genericDataMasking.py

--- src/genericDataMasking.py
+++ src/genericDataMasking.py
@@ -28,16 +28,9 @@ try:
         asset.asset_file_header,
         asset.logger,
     )
-except Exception as e:
-    asset.logger.write(message=str(e))
-    asset.update_data_catalog(data_masking="Failed")
-    asset.logger.write_logs_to_s3()
-
-asset.update_data_catalog(data_masking="In-Progress")
-metadata = asset.get_asset_metadata()
-key = get_secret(asset.secret_name, asset.region, asset.logger)
-result = run_data_masking(source_df, metadata, key, asset.logger)
-try:
+    asset.update_data_catalog(data_masking="In-Progress")
+    metadata = asset.get_asset_metadata()
+    key = get_secret(asset.secret_name, asset.region, asset.logger)
     result = run_data_masking(source_df, metadata, key, asset.logger)
     target_path = asset.get_masking_path()
     target_path = target_path + get_timestamp(asset.source_file_path) + "/"
