7464d3b706c8e2c061d4eebaed94ae59ee5dd0bd
Sagar Das <sagar.das@tigeranalytics.com>
2022-03-01 10:51:04 +0530

    Including changes to update the lambda code only

--- deploy/deploy_lambda.py
+++ deploy/deploy_lambda.py
@@ -20,32 +20,18 @@ def lambda_function_exists(lambda_client, func_name):
     return False
 
 
-def delete_and_create(lambda_client, config, region):
+def update_lambda_code(lambda_client, config, region):
     # TODO: Add lambda permission to include SNS topic after discussion
     fm_prefix = config["fm_prefix"]
     project_name = config["project_name"]
     func_name = config["lambda_function_name"]
     lambda_bucket = f"{fm_prefix}-code-{region}"
     lambda_key = f"{project_name}/lambda/lambda_function.zip"
-    lambda_client.delete_function(FunctionName=func_name)
     try:
-        lambda_client.create_function(
-            Code={
-                "S3Bucket": lambda_bucket,
-                "S3Key": lambda_key,
-            },
-            PackageType="Zip",
-            Description="Testing Lambda deployment",
+        lambda_client.update_function_code(
             FunctionName=func_name,
-            Handler=f"{func_name}.lambda_handler",
-            Role="arn:aws:iam::076931226898:role/service-role/dlFmwrkSrcDataInfo_role",
-            Runtime="python3.9",
-            Environment={
-                "Variables": {
-                    "aws_account": config["aws_account"],
-                    "fm_prefix": config["fm_prefix"],
-                }
-            },
+            S3Bucket=lambda_bucket,
+            S3Key=lambda_key
         )
         return True
     except Exception as e:
@@ -62,13 +48,12 @@ def create_lambda(config, region=None):
     """
     func_name = config["lambda_function_name"]
     lambda_client = boto3.client("lambda", region_name=region)
-    # TODO : Discuss if an end user wants to update the lambda function
     if lambda_function_exists(lambda_client, func_name):
         update = str(input(f"A lambda function by the name {func_name} exists. "
                            f"Do you want to update the lambda function [Y/N]:  "))
         if update.lower() == 'y':
             print("Lambda function is being updated ...")
-            status = delete_and_create(lambda_client, config, region)
+            status = update_lambda_code(lambda_client, config, region)
             return status
         else:
             print("Lambda function is not being updated ...")
