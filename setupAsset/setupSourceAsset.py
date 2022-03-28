aa45bff5f6fa669d1cc9d38400a6790bce1383b3
Sagar Das <sagar.das@tigeranalytics.com>
2022-03-28 10:22:22 +0530

    Formatted changes

--- setupAsset/setupSourceAsset.py
+++ setupAsset/setupSourceAsset.py
@@ -20,35 +20,36 @@ from utils.comUtils import create_asset_catalog_table
 
 
 def main():
-  asset_json_file = sys.argv[1]
-  asset_col_json_file = sys.argv[2]
-  global_config = getGlobalParams()
-  asset_id = int(str(random()).split(".")[1])
+    asset_json_file = sys.argv[1]
+    asset_col_json_file = sys.argv[2]
+    global_config = getGlobalParams()
+    asset_id = int(str(random()).split(".")[1])
 
-  print("Create asset detail table")
-  create_asset_detail_table(asset_id, global_config["primary_region"])
-  create_asset_detail_table(asset_id, global_config["secondary_region"])
+    print("Create asset detail table")
+    create_asset_detail_table(asset_id, global_config["primary_region"])
+    create_asset_detail_table(asset_id, global_config["secondary_region"])
 
-  print("Create asset catalog table")
-  create_asset_catalog_table(asset_id, global_config["primary_region"])
-  create_asset_catalog_table(asset_id, global_config["secondary_region"])
-  time.sleep(10)
+    print("Create asset catalog table")
+    create_asset_catalog_table(asset_id, global_config["primary_region"])
+    create_asset_catalog_table(asset_id, global_config["secondary_region"])
+    time.sleep(10)
 
-  print("Insert asset item")
-  insert_asset_item_dynamoDB(asset_json_file, asset_id, global_config["primary_region"])
-  insert_asset_item_dynamoDB(asset_json_file, asset_id, global_config["secondary_region"])
+    print("Insert asset item")
+    insert_asset_item_dynamoDB(asset_json_file, asset_id, global_config["primary_region"])
+    insert_asset_item_dynamoDB(asset_json_file, asset_id, global_config["secondary_region"])
 
-  print("Insert asset columns")
-  insert_asset_cols_dynamoDB(asset_col_json_file, asset_id, global_config["primary_region"])
-  insert_asset_cols_dynamoDB(asset_col_json_file, asset_id, global_config["secondary_region"])
+    print("Insert asset columns")
+    insert_asset_cols_dynamoDB(asset_col_json_file, asset_id, global_config["primary_region"])
+    insert_asset_cols_dynamoDB(asset_col_json_file, asset_id, global_config["secondary_region"])
 
-  print("Create s3 directory structure")
-  create_src_s3_dir_str(asset_id, asset_json_file, global_config["primary_region"])
-  create_src_s3_dir_str(asset_id, asset_json_file, global_config["secondary_region"])
+    print("Create s3 directory structure")
+    create_src_s3_dir_str(asset_id, asset_json_file, global_config["primary_region"])
+    create_src_s3_dir_str(asset_id, asset_json_file, global_config["secondary_region"])
 
-  print("Add Bucket Notification")
-  set_bucket_event_notification(asset_id, asset_json_file, global_config["primary_region"])
-  set_bucket_event_notification(asset_id, asset_json_file, global_config["secondary_region"])
+    print("Add Bucket Notification")
+    set_bucket_event_notification(asset_id, asset_json_file, global_config["primary_region"])
+    set_bucket_event_notification(asset_id, asset_json_file, global_config["secondary_region"])
 
-if __name__=="__main__":
-  main()
+
+if __name__ == "__main__":
+    main()
