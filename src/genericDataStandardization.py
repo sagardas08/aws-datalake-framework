f58a3134030e4db25a52c369c99bf77d5df76cc1
Racholsan3 <101085979+Racholsan3@users.noreply.github.com>
2022-04-25 11:30:06 +0530

    Update genericDataStandardization.py
    adding job for encryption

--- src/genericDataStandardization.py
+++ src/genericDataStandardization.py
@@ -1,12 +1,21 @@
+import sys
+import time
 from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.context import SparkContext
 from utils.data_asset import DataAsset
 from utils.comUtils import *
 from utils.standardizationUtils import *
-import sys
-import time
+from utils.athena_ddl import get_or_create_db, \
+    get_or_create_table, manage_partition
 
 
 def get_global_config():
+    """
+    Utility method to get global config file
+    :return:JSON
+    """
     config_file_path = "globalConfig.json"
     file = open(file=config_file_path, mode="r")
     config = json.load(file)
@@ -14,12 +23,20 @@ def get_global_config():
     return config
 
 
-spark = SparkSession.builder.appName('Data-Standardization').getOrCreate()
 args = getResolvedOptions(sys.argv, ["source_path", "source_id", "asset_id", "exec_id"])
 global_config = get_global_config()
 start_time = time.time()
+
+# Creating a spark session object
+spark = sql.SparkSession.builder.getOrCreate()
+
+# Creating a job 
+job = Job(GlueContext(SparkContext.getOrCreate()))
+job.init(args["JOB_NAME"], args)
+
 asset = DataAsset(args, global_config, run_identifier="data-standardization")
 try:
+    # Creating spark dataframe from input file.Supported:CSV,Parquet,JSON,ORC
     source_df = create_spark_df(
         spark,
         asset.source_file_path,
@@ -28,34 +45,56 @@ try:
         asset.asset_file_header,
         asset.logger,
     )
-    assert source_df is not None
-except Exception or AssertionError:
-    asset.logger.write(message="Unable to read the data from the source")
+    # Update the data catalog dynamoDB table to "In-Progress" for easy monitoring
+    asset.update_data_catalog(data_standardization="In-Progress")
+    # Getting data asset table dedicated for a specific asset which specifies if masking is required or not
+    metadata = asset.get_asset_metadata()
+    # Function to standardize the data to a user required format
+    result = run_data_standardization(source_df, metadata, asset.logger)
+    # Getting data from target system table
+    target_system_info = get_target_system_info(
+        asset.fm_prefix, asset.target_id, asset.region, asset.logger
+    )
+    # Getting the timestamp identifier from the source path
+    timestamp = get_timestamp(asset.source_path)
+    # Getting the standardization path with the help of info from target system
+    target_path = get_standardization_path(
+        target_system_info, asset.asset_id, timestamp, asset.logger
+    )
+    # Writing the standardized data to the target path in parquet format
+    result.repartition(1).write.parquet(target_path, mode="overwrite")
+
+    # Storing the target file to Athena with DB = Domain and Table = Sub-domain_AssetId
+    domain = target_system_info['domain']
+    get_or_create_db(asset.region, domain, asset.logger)
+    athena_path = get_athena_path(target_system_info, asset.asset_id)
+    
+    # Updating the data catalog table to "Completed" if the standardization is successful
+    asset.update_data_catalog(data_standardization="Completed")
+    
+    #Create table in Athena
+    get_or_create_table(
+        asset.region, result, target_system_info, asset.asset_name,
+        athena_path, partition=True, encrypt=asset.encryption, logger=asset.logger
+    )
+    
+    #Add partitions for the table
+    manage_partition(
+        asset.region, target_system_info, asset.asset_name,
+        timestamp, target_path, asset.logger
+    )
+
+except Exception as e:
+    asset.logger.write(message=str(e))
+    # Updating the data catalog table to "Failed" in case of exceptions
     asset.update_data_catalog(data_standardization="Failed")
     asset.logger.write_logs_to_s3()
-
-asset.update_data_catalog(data_standardization="In-Progress")
-metadata = asset.get_asset_metadata()
-source_df.printSchema()
-result = run_data_standardization(source_df, metadata, asset.logger)
-result.printSchema()
-target_system_info = get_target_system_info(
-    asset.fm_prefix, asset.target_id, asset.region, asset.logger
-)
-timestamp = get_timestamp(asset.source_path)
-target_path = get_standardization_path(
-    target_system_info, asset.asset_id, timestamp, asset.logger
-)
-try:
-    assert result is not None
-    result.repartition(1).write.parquet(target_path, mode="overwrite")
-except AssertionError:
-    asset.update_data_catalog(data_standardization="Failed")
-    asset.logger.write(message="Encountered error while running data standardization")
-
-asset.update_data_catalog(data_standardization="Completed")
+    
 end_time = time.time()
 total_time_taken = float("{0:.2f}".format(end_time - start_time))
 asset.logger.write(message=f"Time Taken = {total_time_taken} seconds")
 asset.logger.write_logs_to_s3()
+# Stop the spark session
 stop_spark(spark)
+# Committing the job
+job.commit()
