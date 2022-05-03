cf6e3c799c931a1e147eb66f05b57d90f4a5c202
Sagar Das <sagar.das@tigeranalytics.com>
2022-05-03 10:54:40 +0530

    migration from dynamoDB to rds code update for setupSource

--- setupAsset/setupSourceAsset.py
+++ setupAsset/setupSourceAsset.py
@@ -7,55 +7,42 @@
 """
 
 import sys
-import os
-import time
-from random import random
-from utils.comUtils import insert_asset_item_dynamoDB
+from random import random, randrange
+from utils.comUtils import insert_asset_item_rds
 from utils.comUtils import getGlobalParams
-from utils.comUtils import create_asset_detail_table
-from utils.comUtils import insert_asset_cols_dynamoDB
+from utils.comUtils import insert_asset_cols_rds
 from utils.comUtils import set_bucket_event_notification
 from utils.comUtils import create_src_s3_dir_str
-from utils.comUtils import create_asset_catalog_table
+
+
+def generate_asset_id(n):
+    return int(f'{randrange(1, 10**n):03}')
 
 
 def main():
+    """
+    Inserting info in the primary region only
+    """
     asset_json_file = sys.argv[1]
     asset_col_json_file = sys.argv[2]
     global_config = getGlobalParams()
-    asset_id = int(str(random()).split(".")[1])
-
-    print("Create asset detail table")
-    create_asset_detail_table(asset_id, global_config["primary_region"])
-    create_asset_detail_table(
-        asset_id, global_config["secondary_region"]
-    )
-
-    print("Create asset catalog table")
-    create_asset_catalog_table(
-        asset_id, global_config["primary_region"]
-    )
-    create_asset_catalog_table(
-        asset_id, global_config["secondary_region"]
-    )
-    time.sleep(10)
-
+    asset_id = generate_asset_id(10)
     print("Insert asset item")
-    insert_asset_item_dynamoDB(
+    insert_asset_item_rds(
         asset_json_file, asset_id, global_config["primary_region"]
     )
-    insert_asset_item_dynamoDB(
-        asset_json_file, asset_id, global_config["secondary_region"]
-    )
-
     print("Insert asset columns")
-    insert_asset_cols_dynamoDB(
+    insert_asset_cols_rds(
         asset_col_json_file, asset_id, global_config["primary_region"]
     )
-    insert_asset_cols_dynamoDB(
+    """
+    insert_asset_item_rds(
+        asset_json_file, asset_id, global_config["secondary_region"]
+    )
+    insert_asset_cols_rds(
         asset_col_json_file, asset_id, global_config["secondary_region"]
     )
-
+    """
     print("Create s3 directory structure")
     create_src_s3_dir_str(
         asset_id, asset_json_file, global_config["primary_region"]
--- setupAsset/setupSourceSystem.py
+++ setupAsset/setupSourceSystem.py
@@ -2,11 +2,12 @@
 == Create a new source system
 1. Insert a new record in the dynamoDb table
 2. Create an S3 bucket associated to the new source system
-3. Create a new IAM user (or othe mechanism) to access the S3 bucket
+3. Create a new IAM user (or other mechanism) to access the S3 bucket
 4. Create a bucket policy accordingly and associate with the S3 bucket
 5. Create an SNS topic associated to the S3 bucket
 6. Subscribe the SNS topic to the DL source system lambda function
 """
+from datetime import datetime, timezone, timedelta
 
 import boto3
 import json
@@ -16,21 +17,17 @@ import sys
 import os
 from random import random
 from utils.comUtils import getGlobalParams
+from connector.pg_connect import Connector
 
 src_sys_id = int(str(random()).split(".")[1])
+global_config = getGlobalParams()
+print(src_sys_id)
 
 
-def insert_src_sys_item_dynamoDB(src_json_file, region):
+def insert_src_sys_info(db, src_json_file, region):
     # TODO: DynamoDB -> RDS: Insert Data
-    global_config = getGlobalParams()
-    dynamodb = boto3.resource("dynamodb", region_name=region)
-    source_system_table = dynamodb.Table(
-        "{}.source_system".format(global_config["fm_prefix"])
-    )
-
     with open(src_json_file) as json_file:
         src_config = json.load(json_file)
-
     bucket_name = (
         global_config["fm_prefix"]
         + "-"
@@ -42,22 +39,20 @@ def insert_src_sys_item_dynamoDB(src_json_file, region):
     mechanism = src_config["mechanism"]
     data_owner = src_config["data_owner"]
     support_cntct = src_config["support_cntct"]
-
-    print(
-        "Insert source system info in {}.source_system table".format(
-            global_config["fm_prefix"]
-        )
-    )
-    response = source_system_table.put_item(
-        Item={
-            "src_sys_id": src_sys_id,
-            "bucket_name": bucket_name,
-            "src_sys_nm": src_sys_nm,
-            "mechanism": mechanism,
-            "data_owner": data_owner,
-            "support_cntct": support_cntct,
-        }
-    )
+    # IST = +5:30
+    current_time = datetime.now(tz=timezone(timedelta(hours=5.5)))
+    table = 'source_system'
+    print(f"Insert source system info in {global_config['fm_prefix']}.{table} table")
+    data = {
+        "src_sys_id": src_sys_id,
+        "bucket_name": bucket_name,
+        "src_sys_nm": src_sys_nm,
+        "mechanism": mechanism,
+        "data_owner": data_owner,
+        "support_cntct": support_cntct,
+        "modified_ts": current_time
+    }
+    db.insert(table=table, data=data)
 
 
 def run_aws_cft(src_json_file, region):
@@ -65,7 +60,6 @@ def run_aws_cft(src_json_file, region):
     method to automate the running of cft
     :return:
     """
-    global_config = getGlobalParams()
     src_sys_cft = "cft/sourceSystem.yaml"
     with open(src_sys_cft) as yaml_file:
         template_body = yaml_file.read()
@@ -102,15 +96,21 @@ def run_aws_cft(src_json_file, region):
 
 
 def main():
-    global_config = getGlobalParams()
-    insert_src_sys_item_dynamoDB(
-        sys.argv[1], global_config["primary_region"]
-    )
-    insert_src_sys_item_dynamoDB(
-        sys.argv[1], global_config["secondary_region"]
-    )
+    db = Connector(global_config['db_secret'], global_config['db_region'])
+    try:
+        insert_src_sys_info(
+            db, sys.argv[1], global_config["primary_region"]
+        )
+        # insert_src_sys_info(
+        #     db, sys.argv[1], global_config["secondary_region"]
+        # )
+    except Exception as e:
+        print(e)
+        db.rollback()
+    finally:
+        db.close()
     run_aws_cft(sys.argv[1], global_config["primary_region"])
-    run_aws_cft(sys.argv[1], global_config["secondary_region"])
+    # run_aws_cft(sys.argv[1], global_config["secondary_region"])
 
 
 if __name__ == "__main__":
